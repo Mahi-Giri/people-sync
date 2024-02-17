096e624341714e32666cc1a785b42256d2060300
Mahi <girimahesh.m@gmail.com>
2024-02-17 16:50:07 +0530

    added gap

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,7 @@ const MovieList = ({ title, movies }) => {
         <div className="mt-12 px-8">
             <h1 className="text-2xl py-4 text-white hover:text-gray-400 cursor-pointer">{title}</h1>
             <div className="card flex overflow-x-scroll">
-                <div className="flex gap-3">
+                <div className="flex gap-4">
                     {movies && movies.map((movie) => <MovieCard key={movie.id} poster_path={movie.poster_path} />)}
                 </div>
             </div>
