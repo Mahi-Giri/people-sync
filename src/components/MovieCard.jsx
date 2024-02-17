27f3efccacb11c68ab5f958d0160e6a748f0d3f7
Mahi <maheshgiri9561@gmail.com>
2024-02-17 22:52:46 +0530

    Change Width of Movie Poster

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,7 @@ import { POSTER_IMAGE } from "../Utils/constant";
 
 const MovieCard = ({ poster_path }) => {
     return (
-        <div className="w-56 h-28 rounded-md">
+        <div className="w-64 rounded-md">
             <img className="w-full h-full cursor-pointer rounded-md" src={POSTER_IMAGE + poster_path} alt="Image_Poster" />
         </div>
     );
